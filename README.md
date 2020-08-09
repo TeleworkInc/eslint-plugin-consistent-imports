6d021e3ad2aab27c414c56ecf6ed43fa3eaa759b
Christian Lewis <ctjl96@gmail.com>
2020-08-09 00:47:38 -0500

    Working order

---
+++ README.md
@@ -0,0 +1,50 @@
+# eslint-plugin-consistent-imports
+
+ESLint rules to enforce consistent variable names for default imports.
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-consistent-imports`:
+
+```
+$ npm install eslint-plugin-consistent-imports --save-dev
+```
+
+
+## Usage
+
+Add `consistent-imports` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "consistent-imports"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "consistent-imports/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/default-variable-match-filename.js
@@ -0,0 +1,44 @@
+/**
+ * @license MIT
+ */
+/**
+ * @fileoverview The variable name for a default import should be a camelcase
+ * alphanumeric version of the filename.
+ *
+ * @author Christian Lewis
+ */
+'use strict';
+
+
+const rule = require('../../../lib/rules/default-variable-match-filename');
+const { DEFAULT_CAMELCASE } = require('../../../lib/camelCase');
+const RuleTester = require('eslint').RuleTester;
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+});
+
+
+const ruleTester = new RuleTester();
+ruleTester.run('default-variable-match-filename', rule, {
+
+  valid: [
+    `import foo from './path/to/foo.js';`,
+    `import myTest from './path/to/myTest.js';`,
+    `import myClass from './path/to/my-class.js';`,
+    `import myPackage from 'my-package'`,
+    `import myZip2 from 'my-zip2'`,
+  ],
+
+  invalid: [
+    {
+      code: `import bar from './path/to/foo.js';`,
+      errors: [{
+        message: DEFAULT_CAMELCASE,
+      }],
+    },
+  ],
+});
